0eabffcb44f5602bc96e55d41ff6ae9dceb4afb3
HJVT <hjvt@hjvt.dev>
2025-03-12 03:16:27 +0300

    Implement

---
+++ PKGBUILD
@@ -0,0 +1,32 @@
+# Maintainer: Ivan Chinenov <ichinenov@hjvt.dev>
+
+pkgname=tstd
+pkgver=0.1.0
+pkgrel=1
+pkgdesc="Command line utility for converting timestamps in various timezones and precisions to human-readable dates"
+url=''
+license=('MIT')
+makedepends=('cargo')
+depends=()
+arch=('i686' 'x86_64' 'armv6h' 'armv7h')
+source=("$pkgname-$pkgver.tar.gz::https://static.crates.io/crates/$pkgname/$pkgname-$pkgver.crate")
+b2sums=()
+
+prepare() {
+    cargo fetch --locked --target "$(rustc -vV | sed -n 's/host: //p')"
+}
+
+build() {
+    export CARGO_TARGET_DIR=target
+    cargo build --frozen --release --all-features
+}
+
+check() {
+    cargo test --frozen --all-features
+}
+
+package() {
+    install -Dm0755 -t "$pkgdir/usr/bin/" "target/release/$pkgname"
+    # for custom license, e.g. MIT
+    # install -Dm644 LICENSE "${pkgdir}/usr/share/licenses/${pkgname}/LICENSE"
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,86 @@
-#[derive(clap::Parser)]
+use clap::{Parser, ValueEnum};
+use time::UtcOffset;
+
+#[derive(Parser)]
+#[command(version, about, author, long_about = None, help_template = "\
+{before-help}{name} v{version}
+
+{about}
+{tab}{author}
+
+{usage-heading} {usage}
+
+{all-args}{after-help}
+")]
 struct Args {
     timestamp: i128,
+    #[arg(long, short, env = "TSTD_RESOLUTION", default_value = "millis")]
+    /// Resolution of the timestamp
     resolution: Resolution,
+
+    #[arg(long, short = 'i', default_value_t = 0)]
+    /// Offset of the <TIMESTAMP> from UTC in hours
+    input_offset: i8,
+
+    #[arg(long, short = 'o', default_value = "local")]
+    /// Which timezone to display the parsed datetime in
+    output_offset: OutputOffset,
 }
 
+#[derive(Clone, Copy, ValueEnum)]
 enum Resolution {
     Secs,
     Millis,
+    Nanos,
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Clone, Copy, ValueEnum)]
+enum OutputOffset {
+    Local,
+    Utc,
+    Original,
+}
+
+fn main() -> Result<(), &'static str> {
+    let args = Args::parse();
+
+    let input_offset =
+        UtcOffset::from_hms(args.input_offset, 0, 0).map_err(|_| "Invalid offset")?;
+
+    let datetime = match args.resolution {
+        Resolution::Secs => time::OffsetDateTime::from_unix_timestamp(
+            args.timestamp
+                .try_into()
+                .map_err(|_| "Timestamp out of range for selected resolution")?,
+        )
+        .map_err(|_| "Could not parse time")?,
+
+        Resolution::Millis => {
+            time::OffsetDateTime::from_unix_timestamp_nanos(args.timestamp * 1000000)
+                .map_err(|_| "Could not parse time")?
+        }
+
+        Resolution::Nanos => time::OffsetDateTime::from_unix_timestamp_nanos(args.timestamp)
+            .map_err(|_| "Could not parse time")?,
+    }
+    .replace_offset(input_offset);
+
+    let datetime = match args.output_offset {
+        OutputOffset::Local => datetime.to_offset(
+            UtcOffset::current_local_offset().map_err(|_| "Can't get local UTC offset")?,
+        ),
+
+        OutputOffset::Utc => datetime.to_offset(UtcOffset::UTC),
+
+        OutputOffset::Original => datetime,
+    };
+
+    println!(
+        "{}",
+        datetime
+            .format(&time::format_description::well_known::Rfc2822)
+            .map_err(|_| "Couldn't format datetime")?
+    );
+
+    Ok(())
 }
